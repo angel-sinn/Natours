8fcbb4caa18b2455a9afdec1ee1c42671c87196b
Angel <angelkysinn@gmail.com>
2021-03-30 23:38:43 -0700

    Set up mongoose schema and model, added some test tours to make sure it connects with compass

--- server.js
+++ server.js
@@ -11,11 +11,43 @@ const DB = process.env.DATABASE.replace(
 
 mongoose
   .connect(DB, {
+    // options to deal with deprecation warnings
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => console.log('DB connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.7,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 197,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => console.log('ERROR!💥:', err));
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
