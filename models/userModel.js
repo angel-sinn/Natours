42599be79b4611f12ac665d409d24cceffb5d8f5
Angel <angelkysinn@gmail.com>
2021-04-02 23:34:05 -0700

    Implemented user signup, login and authentication. Made sure to store hashed pw. Added notes to catchAsync function

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
@@ -18,12 +19,40 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: 8,
+    // will not show up on any query output
+    select: false,
   },
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords do not match',
+    },
   },
 });
+
+// middleware to encrypt password
+userSchema.pre('save', async function (next) {
+  // if current document(user)'s password is not modified
+  if (!this.isModified('password')) return next();
+  // else if password was actually modified, set password to encrypted password
+  this.password = await bcrypt.hash(this.password, 12);
+  // delete password confirmation
+  this.passwordConfirm = undefined;
+  next();
+});
+
+// instance method - available on all documents of a certain collection
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,3 +1,6 @@
+// In order to get rid of try catch statements and keep code clean, we use the catchAsync function that takes in (fn) which is the async function - a promise that needs catch to catch error.
+// This function will return a new anon function which will be assigned to the actual function.
+// If there is an error, it will be caught in the catch statement and will be handled in global handling middleware
 module.exports = (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch((err) => next(err));
