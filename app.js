bed1518a7b33f115998d4943bbd1a831e412e974
Angel <angelkysinn@gmail.com>
2021-04-03 21:31:28 -0700

    Added helmet, express-mongo-sanitize, xss-clean middleware for security http headers and data sanitization

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,21 +13,37 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // GLOBAL MIDDLEWARE
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// set limit for request per time window
+// Set limit for request per time window
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour',
 });
+
+// Limit requests from same API
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection - looks at req.body/param/string and filters out $ and .
+app.use(mongoSanitize());
+
+// Data sanitization against XSS - cleans user input by converting html symbols
+app.use(xss());
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
