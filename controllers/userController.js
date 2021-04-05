dd8dba3d32cfd4674e5c792c04402c8fdf6ed147
Angel <angelkysinn@gmail.com>
2021-04-04 21:30:28 -0700

    Added /me route to get current user info

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,6 +13,12 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
+// For user to get their own info
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
