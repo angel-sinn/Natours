fee9fdb08d51017869460b650adbee03d29216f8
Angel <angelkysinn@gmail.com>
2021-04-05 23:13:00 -0700

    Added geospatial queries for finding tours within distance

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -31,6 +31,8 @@ router
 // /tours-within?distance=233&center=-40,45&unit=mi
 // /tours-within/233/center/34.111745,-118.113491/unit/mi
 
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
 router
   .route('/')
   .get(tourController.getAllTours)
