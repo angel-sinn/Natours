f8f698a051be8d1a51092a06a58258b22456a951
Angel <angelkysinn@gmail.com>
2021-04-06 16:29:24 -0700

    Completed populating tour overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,16 @@
-exports.getOverview = (req, res) => {
+const { find } = require('../models/tourModel');
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+  // 3) Render that template using tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
 };
 
