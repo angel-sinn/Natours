81d7240c0185848585a0c85bc924ea3b6ebc4f43
Angel <angelkysinn@gmail.com>
2021-04-06 19:59:13 -0700

    Completed tour page, refactored code, created _reviewCard partial

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,7 @@
-const { find } = require('../models/tourModel');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = async (req, res, next) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
 
@@ -12,10 +11,18 @@ exports.getOverview = async (req, res, next) => {
     title: 'All Tours',
     tours,
   });
-};
+});
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) Get data for requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  // 2) Build template
+  // 3) Render template using data from 1)
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
