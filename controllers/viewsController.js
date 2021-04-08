327962a3bec62a325128adc2ed69085a841dacb4
Angel <angelkysinn@gmail.com>
2021-04-07 20:06:32 -0700

    Created my account page template and routing. Updated logic so that it is protected only for logged in users

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -49,3 +49,15 @@ exports.getLoginForm = (req, res) => {
       title: 'Log into your acccount',
     });
 };
+
+exports.getAccount = (req, res) => {
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('account', {
+      title: 'Your account',
+    });
+};
