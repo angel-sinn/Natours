340b799555e630058a2dd283cffac81b6b05f34f
Angel <angelkysinn@gmail.com>
2021-04-07 19:39:02 -0700

    Fixed logout so it redirects to homepage

--- public/js/login.js
+++ public/js/login.js
@@ -33,7 +33,8 @@ export const logout = async () => {
       url: 'http://localhost:3000/api/v1/users/logout',
     });
 
-    if ((res.data.status = 'success')) location.reload(true);
+    if ((res.data.status = 'success'))
+      location.replace('http://localhost:3000/');
   } catch (err) {
     showAlert('error', 'Error logging out! Try again.');
   }
